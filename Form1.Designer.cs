506e58e75032f42bb21efe8dc3bcff1e3a8669c5
Nico Deng <88329510+AC3R4PT0R@users.noreply.github.com>
2021-10-05 12:47:15 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace Project_223_Group_21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbx_rm = new System.Windows.Forms.CheckBox();
+            this.cbx_show = new System.Windows.Forms.CheckBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(751, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(751, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 29F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(651, 44);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " Kitted Galore  Management System";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_user.Location = new System.Drawing.Point(916, 323);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(140, 26);
+            this.txt_user.TabIndex = 3;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pass.Location = new System.Drawing.Point(916, 394);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(140, 26);
+            this.txt_pass.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(600, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(698, 150);
+            this.panel1.TabIndex = 5;
+            // 
+            // cbx_rm
+            // 
+            this.cbx_rm.AutoSize = true;
+            this.cbx_rm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_rm.Location = new System.Drawing.Point(881, 493);
+            this.cbx_rm.Name = "cbx_rm";
+            this.cbx_rm.Size = new System.Drawing.Size(144, 24);
+            this.cbx_rm.TabIndex = 6;
+            this.cbx_rm.Text = "Remember me";
+            this.cbx_rm.UseVisualStyleBackColor = true;
+            // 
+            // cbx_show
+            // 
+            this.cbx_show.AutoSize = true;
+            this.cbx_show.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_show.Location = new System.Drawing.Point(1113, 396);
+            this.cbx_show.Name = "cbx_show";
+            this.cbx_show.Size = new System.Drawing.Size(154, 24);
+            this.cbx_show.TabIndex = 7;
+            this.cbx_show.Text = "Show Password";
+            this.cbx_show.UseVisualStyleBackColor = true;
+            this.cbx_show.CheckedChanged += new System.EventHandler(this.cbx_show_CheckedChanged);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(824, 570);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(115, 36);
+            this.btn_clear.TabIndex = 8;
+            this.btn_clear.Text = "Clear All";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(968, 570);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(115, 36);
+            this.btn_login.TabIndex = 9;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(1841, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label4.Location = new System.Drawing.Point(790, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label5.Location = new System.Drawing.Point(844, 426);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label6.Location = new System.Drawing.Point(780, 636);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label7.Location = new System.Drawing.Point(1123, 581);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label8.Location = new System.Drawing.Point(296, 497);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label9.Location = new System.Drawing.Point(1273, 400);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 15);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(1607, 61);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 15);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.cbx_show);
+            this.Controls.Add(this.cbx_rm);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_user);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "frm_login 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox cbx_rm;
+        private System.Windows.Forms.CheckBox cbx_show;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace Project_223_Group_21
+{
+    public partial class Form1 : Form
+    {
+        public bool BRemeberUser = false;
+        public string sRememberUser = "";
+        public string sRememberPassword = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connectStrLogin = @"Data Source=.;Initial Catalog='Kitted Galore Staff';Integrated Security=True";
+        SqlConnection sqlCon;
+        SqlCommand sqlCom;
+        SqlDataReader DataReader;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = false;
+
+            txt_pass.Text = "";
+            txt_pass.PasswordChar = '*';
+            txt_pass.MaxLength = 9;
+            pictureBox1.Image = Project_223_Group_21.Properties.Resources.help_icon;
+
+            sqlCon = new SqlConnection(connectStrLogin);
+            sqlCon.Open();
+            sqlCon.Close();
+
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_user.Clear();
+            txt_pass.Clear();
+            cbx_rm.Checked = false;
+        }
+
+        private void cbx_show_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbx_show.Checked == true)
+            {
+                txt_pass.PasswordChar = '\0';
+            }
+            else {
+                txt_pass.PasswordChar = '*';
+            }
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            int icountusers = 0;
+            int icountindex = 0;
+            string sUsername, sPassword = "";
+
+            if (BRemeberUser == true)
+            {
+                txt_user.Text = sRememberUser;
+                txt_pass.Text = sRememberPassword;
+                BRemeberUser = false;
+            }
+
+            sUsername = txt_user.Text;
+            sPassword = txt_pass.Text;
+
+            sqlCon.Open();
+
+            if (txt_user.Text == "")
+            {
+                MessageBox.Show("Error please enter username");
+            }
+            else if (txt_pass.Text == "")
+            {
+                MessageBox.Show("Error please enter Password");
+            }
+            else
+            {
+                string sqllogin = "SELECT * FROM tbl_login";
+                sqlCom = new SqlCommand(sqllogin, sqlCon);
+                DataReader = sqlCom.ExecuteReader();
+
+                string[] Arrusers = new string[20];
+                string[] Arrpassword = new string[20];
+                string[] Arrlevel = new string[20];
+
+                while (DataReader.Read())
+                {
+                    Arrusers[icountusers] = DataReader.GetValue(0).ToString();
+                    Arrpassword[icountusers] = DataReader.GetValue(1).ToString();
+                    Arrlevel[icountusers] = DataReader.GetValue(2).ToString();
+                    icountusers++;
+                }
+
+                /*for (int i = 0; i < icountusers; i++)
+                  {
+                      listBox1.Items.Add(Arrusers[i]);
+                      listBox1.Items.Add(Arrpassword[i]);
+                      listBox1.Items.Add(Arrlevel[i]);
+                  }*/
+
+                if (cbx_rm.Checked == true)
+                {
+                    BRemeberUser = true;
+                    sRememberUser = txt_user.Text;
+                    sPassword = txt_pass.Text;
+                }
+
+                for (int j = 0; j < icountusers; j++)
+                {
+                    if (Arrusers[j] == sUsername)
+                    {
+                        icountindex = j;
+                    }
+                }
+                if (Arrpassword[icountindex] == sPassword)
+                {
+                    MessageBox.Show("Username : " + sUsername + "Password : " + sPassword + "Confirmed");
+                }
+                else
+                {
+                    MessageBox.Show("Username and or Password is incorrect");
+                }
+            }
+
+
+            sqlCon.Close();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+            label9.Visible = false;
+            label10.Visible = true;
+            label10.Text = "Double click on the help icon to display help,\nSingle click to remove";
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+            label9.Visible = true;
+
+            label4.Text = "Below is the area in which you should enter your designated Username\n                                                    |\n                                                    |\n                                                    |\n                                                    v";
+            label5.Text = "                                ^\n                                |\nAbove is the area that you enter you Password connected to an existing Username";
+            label9.Text = "<---- Click here to see your Password without the protection mask";
+            label8.Text = "Click here to save your Username and Password when you return to the main screen ---->";
+            label7.Text = "<---- After entering your Username and Password click here to login and go to the next form";
+            label6.Text = "                   ^\n                   |\n                   |\n                   |\n Click here to clear all data for the textboxes";
+        }
+    }
+}
